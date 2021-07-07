d76f878242d02b5ea5a3c1b07fabaab92e295a89
Turu0512 <hinataptyan@gmail.com>
2021-07-07 16:07:40 +0900

    完成版

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import Vue from 'vue'
 
+// Style
+import './app.scss'
+
 // Components
-import NumberGuess from './components/NumberGuess.vue'
+import MarkdownPanel from './components/MarkdownPanel.vue'
 
 const app = new Vue({
   el: '#app',
-  data: {
-    message: 'スタートボタンを押してください',
-    tryCountLeft: 0
-  },
   components: {
-    NumberGuess
-  },
-  methods: {
-    onStart (tryCountLeft) {
-      this.message = 'スタート！ 🚀'
-      this.tryCountLeft = tryCountLeft
-    },
-    onCollect (tryCountLeft) {
-      this.message = '当たり！ 🎉'
-      this.tryCountLeft = tryCountLeft
-    },
-    onLower (tryCountLeft) {
-      this.message = 'もっと小さいです 👇'
-      this.tryCountLeft = tryCountLeft
-    },
-    onHigher (tryCountLeft) {
-      this.message = 'もっと大きいです️ ️👆'
-      this.tryCountLeft = tryCountLeft
-    },
-    onLose () {
-      this.message = 'ゲームオーバー 👻'
-      this.tryCountLeft = tryCountLeft
-    }
+    MarkdownPanel
   }
 })
